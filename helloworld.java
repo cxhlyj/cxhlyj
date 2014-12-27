ea23f9045bb811ac95362d2ed00f80435734cb4a
git <git@gmx.com>
2014-12-27 11:50:47 -0500

    create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,17 @@
+<<<<<<< HEAD
+/**add 1 thing Oct.12.2014 modify 2nd times,modify 3rd times,4th times,5th,6th,7th
+=======
+<<<<<<< HEAD
+/**add 1 thing Oct.12.2014 modify 2nd times,modify 3rd times,4th times,5th,6th,other.
+=======
+/**add 1 thing Oct.12.2014 modify 2nd times,modify 3rd times,4th times,5th,6th
+>>>>>>> 7c83e4588ad66b729e02eef307eab45c8285032c
+>>>>>>> 120ea508bdaf83d0abfd36a624c1829909fcb463
+ * The HelloWorldApp class implements an application that
+ * simply prints "Hello World!" to standard output.
+ */
+class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // Display the string.
+    }
+}
